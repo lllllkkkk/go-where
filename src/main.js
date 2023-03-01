43d7b5b60375f101c1252e158d0ad3967d56ac57
liangguangtong <274716548@qq.com>
2023-03-01 09:05:45 +0800

    更新配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/no-unresolved */
 import Vue from 'vue';
 import fastClick from 'fastclick';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './assets/styles/reset.css';
-import './assets/styles/border.css';
-import './assets/styles/iconfont.css';
+import 'styles/reset.css';
+import 'styles/border.css';
+import 'styles/iconfont.css';
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
